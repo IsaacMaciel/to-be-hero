0081703260a363f15e201945ed2a72bc65d5b417
IsaacMaciel <isaac_maciel@hotmail.com>
2020-03-29 09:55:27 -0300

    Terminando a pagina de Logon do Projeto

---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './styles.css';
+import HeroesImg from '../../assets/heroes.png';
+import LogoImg from '../../assets/logo.svg';
+import {FiLogIn} from 'react-icons/fi';
+
+export default function Logon(){
+
+    return (
+       <div className="logon-container">
+           <section className="form">
+               <img src={LogoImg} alt="Be-The-Hero"/>
+
+               <form> 
+                   <h1>Faça seu LOGON</h1>
+                   <input placeholder="Sua ID"/>
+                   <button className="button" type="submit">Entrar</button>
+               <a href="/register"> 
+               <FiLogIn size={16} color="#E02041"/>
+               Não tenho cadastro</a>
+
+             </form>
+
+           </section>
+           <img src={HeroesImg} alt="heroes"/>
+       </div>
+
+    );
+}
